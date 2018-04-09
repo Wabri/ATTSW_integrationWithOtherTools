dc933fb66b770cf72d86be8937b6a259452ee7c2
Wabri <gabriele.puliti@gmail.com>
2018-04-09 18:35:09 +0200

    Done test for StudentService

--- tutorial/src/main/java/attsw/exam/example/core/Student.java
+++ tutorial/src/main/java/attsw/exam/example/core/Student.java
@@ -8,4 +8,8 @@ public class Student {
 		this.id = id;
 	}
 
+	public String getId() {
+		return this.id;
+	}
+
 }
--- tutorial/src/test/java/attsw/exam/example/core/StudentServiceTest.java
+++ tutorial/src/test/java/attsw/exam/example/core/StudentServiceTest.java
@@ -29,18 +29,27 @@ public class StudentServiceTest {
 	public void testAllStudentsWithNoStudents() {
 		verifyNumberOfStudents(0);
 	}
-	
+
 	@Test
 	public void testAllStudents() {
 		studentsList.add(newStudentTest("id0"));
 		studentsList.add(newStudentTest("id1"));
 		verifyNumberOfStudents(2);
 	}
-	
+
 	@Test
 	public void testOneStudentWhenStudentsIsEmpty() {
 		when(repository.findOne("id0")).thenReturn(null);
 		assertNull(studentService.oneStudent("id0"));
+		verify(repository, times(1)).findOne("id0");
+	}
+
+	@Test
+	public void testOneStudent() {
+		when(repository.findOne("id0")).thenReturn(newStudentTest("id0"));
+		Student oneStudent = studentService.oneStudent("id0");
+		assertNotNull(oneStudent);
+		assertEquals("id0", oneStudent.getId());
 		verify(repository,times(1)).findOne("id0");
 	}
 
