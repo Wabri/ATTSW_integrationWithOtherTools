1cb4f2c3a81518d445bd0aace323ad2ccd8ff6f9
Wabri <gabriele.puliti@gmail.com>
2018-04-10 18:14:20 +0200

    now fongo work

---
+++ tutorial/src/main/java/attsw/exam/example/core/repository/mongo/MongoRepository.java
@@ -0,0 +1,20 @@
+package attsw.exam.example.core.repository.mongo;
+
+import java.util.List;
+
+import attsw.exam.example.core.Student;
+import attsw.exam.example.core.repository.Repository;
+
+public class MongoRepository implements Repository {
+
+	@Override
+	public List<Student> findAll() {
+		return null;
+	}
+
+	@Override
+	public Student findOne(String string) {
+		return null;
+	}
+
+}
---
+++ tutorial/src/test/java/attsw/exam/example/core/MongoRepositoryTest.java
@@ -0,0 +1,22 @@
+package attsw.exam.example.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.fakemongo.Fongo;
+
+public class MongoRepositoryTest {
+	
+	@Before
+	public void init() {
+		Fongo fongo = new Fongo("");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
