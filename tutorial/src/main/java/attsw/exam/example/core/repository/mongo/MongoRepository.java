97e868512011cc44b187a5c5976a937f1afec114
Wabri <gabriele.puliti@gmail.com>
2018-05-01 17:27:56 +0200

    create first test for mongo db

--- tutorial/src/main/java/attsw/exam/example/core/repository/mongo/MongoRepository.java
+++ tutorial/src/main/java/attsw/exam/example/core/repository/mongo/MongoRepository.java
@@ -1,15 +1,31 @@
 package attsw.exam.example.core.repository.mongo;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
 
 import attsw.exam.example.core.Student;
 import attsw.exam.example.core.repository.Repository;
 
 public class MongoRepository implements Repository {
 
+	private MongoClient mongoClient;
+	DBCollection students;
+
+	public MongoRepository(MongoClient mongoClient) {
+		this.mongoClient = mongoClient;
+		students = mongoClient.getDB("School").getCollection("Students");
+	}
+
 	@Override
 	public List<Student> findAll() {
-		return null;
+		DBCursor cursor = students.find();
+		return StreamSupport.stream(cursor.spliterator(), false).map(element -> new Student((String) element.get("id")))
+				.collect(Collectors.toList());
 	}
 
 	@Override
--- tutorial/src/test/java/attsw/exam/example/core/MongoRepositoryTest.java
+++ tutorial/src/test/java/attsw/exam/example/core/MongoRepositoryTest.java
@@ -1,22 +1,35 @@
 package attsw.exam.example.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.github.fakemongo.Fongo;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+import attsw.exam.example.core.repository.Repository;
+import attsw.exam.example.core.repository.mongo.MongoRepository;
 
 public class MongoRepositoryTest {
-	
+
+	Repository mongoRepository;
+	DBCollection students;
+
 	@Before
 	public void init() {
-		Fongo fongo = new Fongo("");
+		MongoClient mongoClient = new Fongo("Mongo Server").getMongo();
+		DB db = mongoClient.getDB("School");
+		db.getCollection("Students").drop();
+		mongoRepository = new MongoRepository(mongoClient);
+		students = db.getCollection("Students");
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetAllStudentsEmpty() {
+		assertTrue(mongoRepository.findAll().isEmpty());
 	}
 
 }
