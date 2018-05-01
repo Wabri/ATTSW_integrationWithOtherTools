3cc0aff66c96d3557de0847e3019a130bbc1e6f2
Wabri <gabriele.puliti@gmail.com>
2018-05-01 18:04:24 +0200

    added test for more than one students

--- tutorial/src/test/java/attsw/exam/example/core/MongoRepositoryTest.java
+++ tutorial/src/test/java/attsw/exam/example/core/MongoRepositoryTest.java
@@ -1,11 +1,17 @@
 package attsw.exam.example.core;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import com.github.fakemongo.Fongo;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
@@ -30,6 +36,28 @@ public class MongoRepositoryTest {
 	@Test
 	public void testGetAllStudentsEmpty() {
 		assertTrue(mongoRepository.findAll().isEmpty());
+	}
+	
+	@Test
+	public void testOneStudent() {
+		addStudentToStudentsCollection("id1");
+		assertEquals(1,  mongoRepository.findAll().size());
+	}
+	
+	@Test
+	public void testMoreThanOneStudentsInCollection() {
+		addStudentToStudentsCollection("id1");
+		addStudentToStudentsCollection("id2");
+		List<Student> listOfStudents = mongoRepository.findAll();
+		assertEquals(2, listOfStudents.size());
+		assertEquals("id1", listOfStudents.get(0).getId());
+		assertEquals("id2", listOfStudents.get(1).getId());
+	}
+
+	private void addStudentToStudentsCollection(String idValue) {
+		BasicDBObject document = new BasicDBObject();
+		document.put("id", idValue);
+		students.insert(document);
 	}
 
 }
