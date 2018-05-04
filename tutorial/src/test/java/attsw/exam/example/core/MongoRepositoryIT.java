033d9f3d52c99c462129b61426f14f8fac032038
Wabri <gabriele.puliti@gmail.com>
2018-05-04 15:21:06 +0200

    test with Container and It with docker

--- tutorial/src/test/java/attsw/exam/example/core/MongoRepositoryIT.java
+++ tutorial/src/test/java/attsw/exam/example/core/MongoRepositoryIT.java
@@ -2,20 +2,13 @@ package attsw.exam.example.core;
 
 import java.net.UnknownHostException;
 
-import org.junit.ClassRule;
-import org.testcontainers.containers.GenericContainer;
-
 import com.mongodb.MongoClient;
 
 public class MongoRepositoryIT extends AbstractMongoRepositoryTest {
 
-	@SuppressWarnings("rawtypes")
-	@ClassRule
-	public static GenericContainer mongo = new GenericContainer("mongo:latest").withExposedPorts(27017);
-
 	@Override
 	protected MongoClient extractMongoClient() throws UnknownHostException {
-		return new MongoClient(mongo.getContainerIpAddress(), mongo.getMappedPort(27017));
+		return new MongoClient();
 	}
 
 }
